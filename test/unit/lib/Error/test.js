69ad70141209a9256116fd536eb3175f91f447dd
Eric MORAND <eric.morand@gmail.com>
2018-10-02 00:50:20 +0200

    Fix issue #48

--- test/unit/lib/Error/test.js
+++ test/unit/lib/Error/test.js
@@ -3,12 +3,12 @@ const tap = require('tap');
 
 tap.test('Error', (test) => {
   test.test('constructor', (test) => {
-      let error = new StromboliError('foo', 'bar');
+    let error = new StromboliError('foo', 'bar');
 
-      test.equals(error.file, 'foo');
-      test.equals(error.message, 'bar');
+    test.equals(error.file, 'foo');
+    test.equals(error.message, 'bar');
 
-      test.end();
+    test.end();
   });
 
   test.end();
