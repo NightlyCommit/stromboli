265efc6e946c7eba0770997a3ce18fd7fc0c2bf8
Eric MORAND <eric.morand@gmail.com>
2019-01-02 17:31:08 +0100

    Add entry/output getters to BuildRequest

--- src/lib/Plugin.ts
+++ src/lib/Plugin.ts
@@ -1,9 +1,12 @@
 import {ProcessorInterface} from "./ProcessorInterface";
+import {ComponentInterface} from "./ComponentInterface";
+
+export type PluginEOHandler = (component: ComponentInterface) => string;
 
 export class Plugin {
     readonly name: string;
-    readonly entry: string;
-    readonly output: string;
+    readonly entry: string | PluginEOHandler;
+    readonly output: string | PluginEOHandler;
     readonly processors: ProcessorInterface[];
 
     /**
@@ -12,7 +15,7 @@ export class Plugin {
      * @param output {string}
      * @param processors {ProcessorInterface[]}
      */
-    constructor(name: string, entry: string, output: string, processors: ProcessorInterface[]) {
+    constructor(name: string, entry: string | PluginEOHandler, output: string | PluginEOHandler, processors: ProcessorInterface[]) {
         this.name = name;
         this.entry = entry;
         this.output = output;
--- test/unit/lib/BuildRequest/test.js
+++ test/unit/lib/BuildRequest/test.js
@@ -1,5 +1,5 @@
 const {BuildRequest} = require('../../../../build/cjs/lib/BuildRequest');
-const {ComponentFilesystem, Plugin,Binary} = require('../../../../build/cjs');
+const {ComponentFilesystem, Plugin, Binary} = require('../../../../build/cjs');
 
 const tape = require('tape');
 
@@ -46,6 +46,58 @@ tape.test('BuildRequest', (test) => {
 
       test.end();
     });
+  });
+
+  test.test('entry', (test) => {
+    test.test('handle string', (test) => {
+      let plugin = new Plugin('foo', 'entry', 'output', []);
+
+      let buildRequest = new BuildRequest(component, plugin);
+
+      test.same(buildRequest.entry, 'entry');
+
+      test.end();
+    });
+
+    test.test('handle function', (test) => {
+      let plugin = new Plugin('foo', (component) => {
+        return component.path;
+      }, 'output', []);
+
+      let buildRequest = new BuildRequest(component, plugin);
+
+      test.same(buildRequest.entry, 'foo');
+
+      test.end();
+    });
+
+    test.end();
+  });
+
+  test.test('output', (test) => {
+    test.test('handle string', (test) => {
+      let plugin = new Plugin('foo', 'entry', 'output', []);
+
+      let buildRequest = new BuildRequest(component, plugin);
+
+      test.same(buildRequest.output, 'output');
+
+      test.end();
+    });
+
+    test.test('handle function', (test) => {
+      let plugin = new Plugin('foo', 'entry', (component) => {
+        return component.path;
+      }, []);
+
+      let buildRequest = new BuildRequest(component, plugin);
+
+      test.same(buildRequest.output, 'foo');
+
+      test.end();
+    });
+
+    test.end();
   });
 
   test.end();
